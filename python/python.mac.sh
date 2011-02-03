614cfdb15885b2830f655115f53a7215d76c350b
Zeathe <zeathe@qua-axiom.com>
2011-02-02 22:58:10 -0800

    fixed Path Exporting to be definitive

--- python/python.mac.sh
+++ python/python.mac.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+pushd $(dirname $(which ${BASH_SOURCE[0]}))
+
 pushd ../MacOSXx86_64/Python/v2.6
 export PYTHONBIN=`pwd`/bin/python
 export PYTHONPATH=`pwd`/lib
+popd
+
 popd
 
 ${PYTHONBIN} $*
