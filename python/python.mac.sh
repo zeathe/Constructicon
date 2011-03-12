4022aee3b4e30540b74599b075885963ac404864
Zeathe <zeathe@qua-axiom.com>
2011-03-12 08:42:55 -0800

    Cleaning up python directory
    	- Removed all non-essential LIBs
    	- Attempting to remove Python Binaries from
    	  Constructicon

--- python/python.mac.sh
+++ python/python.mac.sh
@@ -2,13 +2,14 @@
 
 pushd $(dirname $(which ${BASH_SOURCE[0]}))
 
-pushd ../MacOSXx86_64/Python/v2.6
-export PYTHONBIN=`pwd`/bin/python
-export PYTHONPATH=`pwd`/lib
-popd
+#pushd ../MacOSXx86_64/Python/v2.6
+#export PYTHONBIN=`pwd`/bin/python
+#export PYTHONPATH=`pwd`/lib
+#popd
 
 export PYTHONPATH=${PYTHONPATH}:`pwd`/lib
 
 popd
 
-${PYTHONBIN} $*
+#${PYTHONBIN} $*
+python $*
